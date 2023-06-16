6696cef8c3039e4f413df954d8f4d479f7ca4a55
denissemaiz <denissemaiz@gmail.com>
2023-06-16 19:46:55 -0300

    Agrego conexión a BD Articulos para Productos

--- tpCuatrimestral/eCommerce/Productos.aspx.cs
+++ tpCuatrimestral/eCommerce/Productos.aspx.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
+using negocio;
 
 namespace eCommerce
 {
     public partial class Productos : System.Web.UI.Page
     {
+        public List<Articulo> listaArticulos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ArticuloNegocio articulo = new ArticuloNegocio();
+            List<Articulo> listaSinRepetidos = articulo.RemoveDuplicadosArticulo(articulo.Listar());
+            repArticulos.DataSource = listaSinRepetidos;
+            repArticulos.DataBind();
         }
     }
 }
