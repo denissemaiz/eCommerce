b674041fe7f515bb3d67a285e01c57f215978eb1
denissemaiz <denissemaiz@gmail.com>
2023-07-05 22:58:00 -0300

    Reemplazo ProductosADM por Productos

--- tpCuatrimestral/eCommerce/Productos.aspx.cs
+++ tpCuatrimestral/eCommerce/Productos.aspx.cs
@@ -4,20 +4,60 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Clases;
 using dominio;
 using negocio;
+
 
 namespace eCommerce
 {
     public partial class Productos : System.Web.UI.Page
     {
-        public List<Libro> listaArticulos { get; set; }
+
+        public List<Libro> ListarLibros { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            LibroNegocio articulo = new LibroNegocio();
-            List<Libro> listaSinRepetidos = articulo.RemoveDuplicadosLibro(articulo.Listar());
-            repArticulos.DataSource = listaSinRepetidos;
-            repArticulos.DataBind();
+            if (!IsPostBack)
+            {
+                LibroNegocio negocio = new LibroNegocio();
+                ListarLibros = negocio.ListarL();
+                Repetidor.DataSource = ListarLibros;
+                Repetidor.DataBind();
+            }
+            
+        }
+
+        protected void btnEliminarLibro_Click(object sender, EventArgs e)
+        {
+            string idLibro = ((Button)sender).CommandArgument;
+
+            LibroNegocio articulos = new LibroNegocio();
+            ListarLibros = articulos.PruebaBuscar(idLibro);
+
+            if (ListarLibros != null)
+            {
+                LibroNegocio negocio = new LibroNegocio();
+                int id = Convert.ToInt32(idLibro);
+                negocio.Eliminar(id);
+                Response.Redirect("Productos.aspx");
+            }
+        }
+
+        protected void btnModificarLibro_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("StoreProc.aspx");
+        }
+
+        public bool ValidarAdmin()
+        {
+            Usuario user;
+            if (Session["Usuario"] != null)
+            {
+                user = ((Usuario)Session["Usuario"]);
+                return user.EsAdmin;
+            }
+            return false;
         }
     }
 }
