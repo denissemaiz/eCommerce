d09a2e12cc5c960cd08a3b6a014251ce54999194
Alan <alaanoriel@gmail.com>
2023-07-16 23:27:28 -0300

    Form cambiar contraseña, recuperar contraseña

---
+++ tpCuatrimestral/eCommerce/User/CambiarContraseña.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User/User.Master" AutoEventWireup="true" CodeBehind="CambiarContraseña.aspx.cs" Inherits="eCommerce.User.CambiarContraseña" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <form class="">
+        <div class="row m-3 justify-content-center">
+            <div class="col-md-6">
+                <asp:Label ID="LblContraseñaActual" runat="server" CssClass="form-label"><b>Contraseña actual</b></asp:Label>
+                <asp:TextBox ID="TxtContraseñaActual" runat="server" CssClass="form-control" TextMode="Password" placeholder="*********"></asp:TextBox>
+            </div>
+        </div>
+        <div class="row m-3 justify-content-center">
+            <div class="col-md-6">
+                <asp:Label ID="LblContraseñaNueva" runat="server" CssClass="form-label"><b>Nueva contraseña</b></asp:Label>
+                <asp:TextBox ID="txtContraseñaNueva" runat="server" CssClass="form-control" TextMode="Password" placeholder="*********"></asp:TextBox>
+            </div>
+        </div>
+        <div class="row m-3 justify-content-center">
+            <div class="col-md-6">
+                <asp:Label ID="lblContraseñaConfirmar" runat="server" CssClass="form-label"><b>Confirmar contraseña</b> </asp:Label>
+                <asp:TextBox ID="txtContraseñaNuevaConfirmar" runat="server" CssClass="form-control" TextMode="Password" placeholder="*********"></asp:TextBox>
+                <br />
+                <a href="..\Default.aspx" class="btn btn-secondary">Cancelar</a>
+                <asp:Button ID="btnAceptar" runat="server" Text="Aceptar" CssClass="btn btn-primary"/>
+            </div>
+        </div>
+    </form>
+
+</asp:Content>
---
+++ tpCuatrimestral/eCommerce/User/RecuperarContraseña.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace eCommerce.User
+{
+
+
+    public partial class RecuperarContraseña
+    {
+
+        /// <summary>
+        /// Control LblContraseña.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblContraseña;
+
+        /// <summary>
+        /// Control txtContraseñaNueva.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtContraseñaNueva;
+
+        /// <summary>
+        /// Control lblContraseñaConfirmar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblContraseñaConfirmar;
+
+        /// <summary>
+        /// Control txtContraseñaNuevaConfirmar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtContraseñaNuevaConfirmar;
+
+        /// <summary>
+        /// Control btnAceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAceptar;
+    }
+}
