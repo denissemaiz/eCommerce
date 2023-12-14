cfbf46fdc46b775f4da08b3da75374b7f1976839
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-12-14 19:35:32 -0300

    Creacion del stored procedure sp_VentasTotalesPorMes_Anio

---
+++ Db_Querys/SP_Reportes.sql
@@ -0,0 +1,43 @@
+CREATE PROCEDURE sp_VentasTotalesPorMes_Anio
+    @Anio INT,
+    @ColumnaOrderBy NVARCHAR(50) = 'Mes',
+    @OrdenarPor NVARCHAR(4) = 'ASC'
+AS
+BEGIN
+    DECLARE @OrderBy NVARCHAR(100);
+
+    --Valido input para evitar inyección SQL
+    SET @ColumnaOrderBy = CASE 
+        WHEN @ColumnaOrderBy IN ('Mes', 'MontoTotalMes', 'NumeroDeVentas') THEN @ColumnaOrderBy
+        ELSE 'Mes'
+    END;
+
+    SET @OrdenarPor = UPPER(@OrdenarPor);
+    SET @OrdenarPor = CASE 
+        WHEN @OrdenarPor IN ('ASC', 'DESC') THEN @OrdenarPor
+        ELSE 'ASC'
+    END;
+
+    SET @OrderBy = QUOTENAME(@ColumnaOrderBy) + ' ' + @OrdenarPor;
+
+    DECLARE @SQLQuery NVARCHAR(MAX);
+
+    SET @SQLQuery = N'
+        SELECT
+            MONTH(FechaCompra) AS Mes,
+            SUM(PrecioTotal) AS MontoTotalMes,
+            COUNT(*) AS NumeroDeVentas
+        FROM Compra
+        WHERE YEAR(FechaCompra) = @ParamAnio
+        GROUP BY MONTH(FechaCompra)
+        ORDER BY ' + @OrderBy + ';';
+
+    EXEC sp_executesql @SQLQuery, N'@ParamAnio INT', @Anio;
+END;
+
+--EXEC sp_VentasTotalesPorMes_Anio
+--    @Anio = 2023,
+--    @ColumnaOrderBy = 'MontoTotalMes',
+--    @OrdenarPor = 'DESC';
+
+--DROP PROCEDURE IF EXISTS sp_VentasTotalesPorMes_Anio
