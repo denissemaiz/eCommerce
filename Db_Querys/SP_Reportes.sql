8fcad1c41cb8fa85fa07efe1d5a2647dd843e5a8
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-12-15 13:54:23 -0300

    Commit provisorio

--- Db_Querys/SP_Reportes.sql
+++ Db_Querys/SP_Reportes.sql
@@ -6,7 +6,7 @@ AS
 BEGIN
     DECLARE @OrderBy NVARCHAR(100);
 
-    -- Validar input para evitar inyección SQL
+    --Valido input para evitar inyección SQL
     SET @ColumnaOrderBy = CASE 
         WHEN @ColumnaOrderBy IN ('Mes', 'MontoTotalMes', 'NumeroDeVentas') THEN @ColumnaOrderBy
         ELSE 'Mes'
@@ -24,20 +24,21 @@ BEGIN
 
     SET @SQLQuery = N'
         SELECT
-            FORMAT(FechaCompra, ''MMMM'') AS Mes,
+            MONTH(FechaCompra) AS Mes,
             SUM(PrecioTotal) AS MontoTotalMes,
             COUNT(*) AS NumeroDeVentas
         FROM Compra
         WHERE YEAR(FechaCompra) = @ParamAnio AND ID_Estado <> 4
-        GROUP BY FORMAT(FechaCompra, ''MMMM'')
+        GROUP BY MONTH(FechaCompra)
         ORDER BY ' + @OrderBy + ';';
 
     EXEC sp_executesql @SQLQuery, N'@ParamAnio INT', @Anio;
 END;
+
 
 EXEC SP_VentasTotalesPorMes_Anio
     @Anio = 2023,
     @ColumnaOrderBy = 'Mes',
     @OrdenarPor = 'DESC';
 
---DROP PROCEDURE IF EXISTS SP_VentasTotalesPorMes_Anio
+DROP PROCEDURE IF EXISTS SP_VentasTotalesPorMes_Anio
